0d275176c8205d15c570a03eeb7ce0ce33ce3f74
Nihal Ahmed <nihaltomail@gmail.com>
2021-05-23 07:50:51 +0530

    Update module.js

--- module.js
+++ module.js
@@ -14,6 +14,20 @@ Modules can load each other and use special directives export and import to inte
 export keyword labels variables and functions that should be accessible from outside the current module.
 import allows the import of functionality from other modules.
 
+To summarize, the core concepts are:
 
+A module is a file. To make import/export work, browsers need <script type="module">. Modules have several differences:
+    Deferred by default.
+    Async works on inline scripts.
+    To load external scripts from another origin (domain/protocol/port), CORS headers are needed.
+    Duplicate external scripts are ignored.
+    Modules have their own, local top-level scope and interchange functionality via import/export.
+    Modules always use strict.
+Module code is executed only once. Exports are created once and shared between importers.
+When we use modules, each module implements the functionality and exports it. Then we use import to directly import it where it’s needed. The browser loads and evaluates the scripts automatically.
+
+In production, people often use bundlers such as Webpack to bundle modules together for performance and other reasons.
+
+In the next chapter we’ll see more examples of modules, and how things can be exported/imported.
 
 */
